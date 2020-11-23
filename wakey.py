b97b5804b4937c1c619646382ae9367719d0fee0
cjerrington <me@claytonerrington.com>
2020-11-23 12:03:19 -0600

    Adding program files

---
+++ wakey.py
@@ -0,0 +1,20 @@
+import keyboard, time, sys
+import ctypes
+import os
+
+if sys.platform == "win32":
+    cmd = 'mode 30,10'
+    os.system(cmd)
+    ctypes.windll.kernel32.SetConsoleTitleW('Wakey')
+
+
+print("Keeping computer awake :)")
+
+while True:
+    try:
+        keyboard.press_and_release('f15')
+        delay = 300 # 5 minutes
+        time.sleep(300)  # Sleep for the amount of seconds generated
+    except KeyboardInterrupt:
+        # quit
+        sys.exit()
